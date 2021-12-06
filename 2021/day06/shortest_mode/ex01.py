06b834af999a63ad2f017fcfa55cc56e58d01325
Abel van Bergen en Henegouwen <abelfranciscusvanbergen@MacBook-Pro-van-Abel.local>
2021-12-06 16:23:37 +0100

    nog korter

--- 2021/day06/shortest_mode/ex01.py
+++ 2021/day06/shortest_mode/ex01.py
@@ -1,4 +1,3 @@
-f=[0]*9
-for x in open("i").read()[::2]:f[int(x)]+=1
-for d in range(256):f[7]+=f[0];f=f[1:]+f[0:1]
+f=[*map(open("i").read().count,"012345678")]
+for _ in[0]*256:f[7]+=f[0];f=f[1:]+f[:1]
 print(sum(f))

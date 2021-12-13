51c7d377f3dfdb33d6c884f54facc7145f82d327
Abel Van bergen en henegouwen <avan-ber@f0r5s11.codam.nl>
2021-12-13 09:18:03 +0100

    making day13 a bit cleaner

--- 2021/day13/ex01.py
+++ 2021/day13/ex01.py
@@ -1,20 +1,19 @@
+def flip_coor(coor, char, foldline):
+	x, y = coor
+	if char == "x" and x > foldline:
+		x = 2*foldline - x
+	elif char == "y" and y > foldline:
+		y = 2*foldline - y
+	return (x, y)
+
 coor, folds = open("input.txt").read().split("\n\n")
 coors = {(int(l.split(',')[0]), int(l.split(',')[1])) for l in coor.split()}
 for f in folds.split('\n'):
 	new_coor = set()
 	foldline = int(f[f.index('=') + 1:])
-	if "x" in f:
-		for c in coors:
-			x, y = c
-			if (x > foldline):
-				x = foldline - (x - foldline)
-			new_coor.add((x, y))
-	else:
-		for c in coors:
-			x, y = c
-			if (y > foldline):
-				y = foldline - (y - foldline)
-			new_coor.add((x, y))
+	fold_dir = f[f.index('=') - 1]
+	for c in coors:
+		new_coor.add(flip_coor(c, fold_dir, foldline))
 	coors = new_coor
 	print(len(coors))
 	quit()
--- 2021/day13/ex02.py
+++ 2021/day13/ex02.py
@@ -1,25 +1,22 @@
+def flip_coor(coor, char, foldline):
+	x, y = coor
+	if char == "x" and x > foldline:
+		x = 2*foldline - x
+	elif char == "y" and y > foldline:
+		y = 2*foldline - y
+	return (x, y)
+
 coor, folds = open("input.txt").read().split("\n\n")
 coors = {(int(l.split(',')[0]), int(l.split(',')[1])) for l in coor.split()}
 for f in folds.split('\n')[:-1]:
 	new_coor = set()
 	foldline = int(f[f.index('=') + 1:])
-	if "x" in f:
-		for c in coors:
-			x, y = c
-			if (x > foldline):
-				x = foldline - (x - foldline)
-			new_coor.add((x, y))
-	else:
-		for c in coors:
-			x, y = c
-			if (y > foldline):
-				y = foldline - (y - foldline)
-			new_coor.add((x, y))
+	fold_dir = f[f.index('=') - 1]
+	for c in coors:
+		new_coor.add(flip_coor(c, fold_dir, foldline))
 	coors = new_coor
-grid = []
-for _ in range(7):
-	grid.append(["."] * 40)
-for x, y in coors:
-	grid[y][x] = "#"
-for line in grid:
-	print("".join(line))
+
+max_x = max(c[0] for c in coors)
+max_y = max(c[1] for c in coors)
+for y in range(max_y + 1):
+	print("".join("#" if (x, y) in coors else " " for x in range(max_x + 1)))

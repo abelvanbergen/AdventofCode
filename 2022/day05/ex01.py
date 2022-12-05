5c50f045eb6f29ed6fd57d00a7b6b3053d0a7add
abelvanbergen <afvanbergen@live.nl>
2022-12-05 13:09:56 +0100

    day 5 and a small change

---
+++ 2022/day05/ex01.py
@@ -0,0 +1,14 @@
+state, instructions = [l.split('\n') for l in open("input.txt").read().split('\n\n')]
+stacks = [[] for _ in state[-1].split()]
+for line in state[-2::-1]:
+	j = 0
+	for i in range(1,len(line),4):
+		if (line[i] != ' '):
+			stacks[j].append(line[i])
+		j += 1
+for line in instructions[:-1]:
+	tokens = line.split()
+	amount, src, dest = [int(tokens[i]) for i in range(1,len(tokens), 2)]
+	stacks[dest-1] += stacks[src-1][:-amount-1:-1]
+	stacks[src-1] = stacks[src-1][:-amount]
+print("".join(x[-1] for x in stacks))

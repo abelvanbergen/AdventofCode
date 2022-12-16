8e77d975fd29545b52835684f8dfd62f1b3b520d
Abel Van bergen en henegouwen <afvanbergen@live.nl>
2022-12-16 14:55:42 +0100

    next try

--- 2022/day15/ex02.py
+++ 2022/day15/ex02.py
@@ -1,37 +1,46 @@
+class Formula:
+	def __init__(self, a, coor, length):
+		self.a = a
+		self.b = coor[1] - coor[1] * a
+		self.coor = coor
+		self.length = length
+	
+	def y_of_x(self, x):
+		return (self.a * x + self.b)
+
+	def intersect(self, obj)
+		if (self.a == obj.a):
+			return (None)
+		return ((-self.b + obj.b) // 32)
+
+
 def manhatten_distance(a, b):
 	return abs(a[0] - b[0]) + abs(a[1] - b[1])
 
 sensors = set()
-for line in open("example.txt").read().splitlines():
+for line in open("input.txt").read().splitlines():
 	tokens = line.split()
 	sx, sy, bx = [int(tokens[i][2:-1]) for i in [2, 3, 8]]
 	by = int(tokens[9][2:])
 	mh_dis = manhatten_distance((sx, sy), (bx, by))
 	sensors.add((sx, sy, mh_dis))
 
-new_coor = set()
-for ax, ay, am in sensors:
-	for bx, by, bm in sensors:
-		if manhatten_distance((ax, ay), (bx, by)) == am + bm + 2:
-			new_coor.add((ax, ay, am))
-			new_coor.add((bx, by, bm))
-print(new_coor)
+formula = set()
+for sensor in sensors
 
-overlap = dict()
-for sensor in new_coor:
-	for dy in range(-sensor[2], sensor[2] + 1):
-		for dx in range(-sensor[2], sensor[2] + 1):
-			if (manhatten_distance((sensor[0] + dx, sensor[1] + dy), (sensor[0], sensor[1])) <= sensor[2] + 1):
-				if (sensor[0] + dx, sensor[1] + dy) in overlap:
-					overlap[(sensor[0] + dx, sensor[1] + dy)] += 1
-				else:
-					overlap[(sensor[0] + dx, sensor[1] + dy)] = 1
-for key in overlap.keys():
-	print(key)
-	overlap = False
+for i in range(4_000_000):
+	print(i)
+	line_x = set()
 	for sensor in sensors:
-		if manhatten_distance(key, (sensor[0], sensor[1])) <= sensor[2]:
-			overlap = True
-	if not overlap:
-		print(key)
-		break
+		distance_to_line = abs(sensor[1] - i)
+		start = sensor[0] - (sensor[2] - distance_to_line)
+		if start < 0:
+			start = 0
+		end = sensor[0] + (sensor[2] - distance_to_line)
+		if end > 4_000_000:
+			end = 4_000_000
+		for x in range(start, end):
+			line_x.add(x)
+	if (len(line_x) < 4_000_000):
+		print(i)
+		quit()
